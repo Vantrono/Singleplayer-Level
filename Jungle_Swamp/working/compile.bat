e938d6778ca821c2323ca5ceb83e0dbb5ee2e789
Vantrono <61304278+Vantrono@users.noreply.github.com>
2021-11-16 20:55:16 -0600

    Metrics

--- Jungle_Swamp/working/compile.bat
+++ Jungle_Swamp/working/compile.bat
@@ -1,22 +1,22 @@
 @echo off
 
 cd\
-cd C:\QuakeDev\working
+cd C:\Users\382985\Documents\GitHub\Singleplayer-Level\Jungle_Swamp\working
 
 
 echo Copying Files...
-copy C:\Users\382985\Documents\GitHub\Qwoke\QuakeDev\id1\maps\SplitQwake.map C:\QuakeDev\working
+copy C:\Users\382985\Documents\GitHub\Singleplayer-Level\Jungle_Swamp\id1\maps\Metrics_and_EnemyTestLevel.map C:\Users\382985\Documents\GitHub\Singleplayer-Level\Jungle_Swamp\working
 
 
 echo Converting map...
 
 
 echo --------------QBSP--------------
-C:\QuakeDev\tools\ericw-tools\bin\qbsp.exe SplitQwake
+C:\QuakeDev\tools\ericw-tools\bin\qbsp.exe Metrics_and_EnemyTestLevel
 
 echo --------------VIS---------------
-C:\QuakeDev\tools\ericw-tools\bin\vis.exe SplitQwake
+C:\QuakeDev\tools\ericw-tools\bin\vis.exe Metrics_and_EnemyTestLevel
 
-copy SplitQwake.bsp C:\QuakeDev\id1\maps
-copy SplitQwake.pts C:\QuakeDev\id1\maps
-copy SplitQwake.lit C:\QuakeDev\id1\maps
+copy Metrics_and_EnemyTestLevel.bsp C:\Users\382985\Documents\GitHub\Singleplayer-Level\Jungle_Swamp\id1\maps
+copy Metrics_and_EnemyTestLevel.pts C:\Users\382985\Documents\GitHub\Singleplayer-Level\Jungle_Swamp\id1\maps
+copy Metrics_and_EnemyTestLevel.lit C:\Users\382985\Documents\GitHub\Singleplayer-Level\Jungle_Swamp\id1\maps
--- Jungle_Swamp/working/run.bat
+++ Jungle_Swamp/working/run.bat
@@ -1,3 +1,3 @@
 cd\
-cd C:\QuakeDev
-quakespasm-spiked-win64  +map SplitQwake
+cd C:\Users\382985\Documents\GitHub\Singleplayer-Level\Jungle_Swamp
+quakespasm-spiked-win64  +map Metrics_and_EnemyTestLevel
