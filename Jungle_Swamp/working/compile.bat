28666f15c4e900cfb9908d41466e8ea9d787f8d0
Vantrono <382985@colum.edu>
2021-12-14 15:14:23 -0600

    Final

--- Jungle_Swamp/working/compile.bat
+++ Jungle_Swamp/working/compile.bat
@@ -14,6 +14,15 @@ echo Converting map...
 echo --------------QBSP--------------
 C:\QuakeDev\tools\ericw-tools\bin\qbsp.exe Sleepy_Swamp
 
+echo --------------VIS---------------
+C:\QuakeDev\tools\ericw-tools\bin\vis.exe Sleepy_Swamp
+
+echo -------------LIGHT--------------
+C:\QuakeDev\tools\ericw-tools\bin\light.exe -soft -extra4 Sleepy_Swamp
+
 copy Sleepy_Swamp.bsp C:\Users\Dat\Documents\Sleepy-Swamp\Jungle_Swamp\id1\maps
 copy Sleepy_Swamp.pts C:\Users\Dat\Documents\Sleepy-Swamp\Jungle_Swamp\id1\maps
 copy Sleepy_Swamp.lit C:\Users\Dat\Documents\Sleepy-Swamp\Jungle_Swamp\id1\maps
+cd\
+cd C:\Users\Dat\Documents\Sleepy-Swamp\Jungle_Swamp
+quakespasm-spiked-win64  +map Sleepy_Swamp
